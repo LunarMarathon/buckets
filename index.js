95115c9a22398b5eb2e703e3d2d75beb28a458e4
LunarMarathon <lmaytan24@gmail.com>
2023-03-18 10:48:02 +0530

    added drag and drop functionality
    https://imgur.com/a/OvrqMO8
    uuid cdn for list ids

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, push, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 const taskInputEl = document.getElementById("taskInputEl")
 const mainTaskList = document.getElementById("mainTaskList")
 const addBtn = document.getElementById("addBtn")
+const allBuckets = document.querySelectorAll(".buckets")
+// const allListItems = document.querySelectorAll(".items")
 
 const appSettings = {
     databaseURL: ""
@@ -21,7 +23,7 @@ addBtn.addEventListener("click", function () {
 })
 
 onValue(tasklistInDB, function (snapshot) {
-    mainTaskList.innerHTML=""
+    mainTaskList.innerHTML = ""
     if (snapshot.val()) {
         let taskArray = Object.values(snapshot.val())
         // alert(taskArray)
@@ -33,6 +35,31 @@ function appendTaskUl(taskArray) {
     for (let task of taskArray) {
         let listEl = document.createElement("li")
         listEl.textContent = task
+        listEl.setAttribute("draggable", "true")
+        listEl.setAttribute("id", uuidv4())
+        listEl.classList.add("listItems")
         mainTaskList.append(listEl)
     }
 }
+function handleDrag(e) {
+    // to avoid the input elements or the buckets
+    if (e.target.classList.contains("listItems"))
+        e.dataTransfer.setData("text", e.target.id)
+}
+function handleDragOver(e) {
+    e.preventDefault()
+}
+function handleDrop(e) {
+    e.preventDefault()
+    let data = e.dataTransfer.getData("text")
+    // to avoid appending in a task
+    if (e.target.classList.contains("buckets"))
+        e.target.appendChild(document.getElementById(data))
+}
+
+// using allListItems doesn't allow dropping into buckets
+allBuckets.forEach(function (bucket) {
+    bucket.addEventListener("dragstart", handleDrag)
+    bucket.addEventListener("dragover", handleDragOver)
+    bucket.addEventListener("drop", handleDrop)
+})
