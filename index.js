6f1836429c95d9e744244d761f2c7130bd7de217
LunarMarathon <lmaytan24@gmail.com>
2023-03-19 08:49:01 +0530

    ref for bucket, push, onValue, changed layout(10)
    https://imgur.com/a/rdxKQZc
    Yet to refactor and add remove functionality + ref for all buckets

--- index.js
+++ index.js
@@ -17,12 +17,22 @@ const appSettings = {
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const tasklistInDB = ref(database, "bucketTasks")
+const bucketlistInDB = ref(database, "indiBucketTasks")
+let bucketArray = []
 
 addBtn.addEventListener("click", pushIntoDB)
 
 function pushIntoDB() {
-    push(tasklistInDB, taskInputEl.value)
-    taskInputEl.value = ""
+    if (taskInputEl.value) {
+        push(tasklistInDB, taskInputEl.value)
+        taskInputEl.value = ""
+    }
+    else {
+        taskInputEl.placeholder = "Fill this field"
+        setTimeout(function () {
+            taskInputEl.placeholder = "Add task"
+        }, 1500)
+    }
 }
 
 onValue(tasklistInDB, function (snapshot) {
@@ -45,6 +55,7 @@ function appendTaskUl(taskArray) {
         mainTaskList.append(listEl)
     }
 }
+
 function handleDrag(e) {
     // to avoid the input elements or the buckets
     if (e.target.classList.contains("listItems"))
@@ -58,7 +69,8 @@ function handleDrop(e) {
     let data = e.dataTransfer.getData("text")
     // to avoid appending in a task
     if (e.target.classList.contains("buckets"))
-        e.target.appendChild(document.getElementById(data))
+        // e.target.appendChild(document.getElementById(data))
+        push(bucketlistInDB, document.getElementById(data).textContent)
 }
 
 // using allListItems doesn't allow dropping into buckets
@@ -66,6 +78,8 @@ allBuckets.forEach(function (bucket) {
     bucket.addEventListener("dragstart", handleDrag)
     bucket.addEventListener("dragover", handleDragOver)
     bucket.addEventListener("drop", handleDrop)
+    // It's better to push when the task is dropped itself
+    // bucket.addEventListener("DOMSubtreeModified", handleChange)
 })
 
 function handleTaskClick() {
@@ -108,3 +122,57 @@ document.addEventListener("keydown", function (e) {
             break
     }
 })
+
+// function handleChange() {
+//     if (this.id != "b5") {
+//         // console.log("changed")
+//         // alert("changed")
+//         let tasksArray = document.getElementById(this.id).getElementsByTagName("li")
+//         // for (let a of tasksArray) {
+//         //     console.log(a)
+//         // }
+//         let lastTask = tasksArray[tasksArray.length - 1].textContent
+//         push(bucketlistInDB, lastTask)
+//     }
+// }
+
+// onValue(bucketlistInDB, function (snapshot) {
+//     bucketArray = Object.values(snapshot.val())
+//     console.log(bucketArray)
+//     // document.getElementById("b1").innerHTML=""
+// })
+
+
+// for (let task of bucketArray){
+//     console.log(task)
+//     let listEl = document.createElement("li")
+//     listEl.textContent = task
+//     listEl.setAttribute("draggable", "true")
+//     listEl.setAttribute("id", uuidv4())
+//     listEl.classList.add("listItems")
+//     listEl.addEventListener("click", handleTaskClick)
+//     document.getElementById("b1").append(listEl)
+// }
+
+onValue(bucketlistInDB, function(snapshot){
+    fetch(snapshot, "b1")
+})
+
+function appendBucket(taskArray, bucketId) {
+    for (let task of taskArray) {
+        let listEl = document.createElement("li")
+        listEl.textContent = task
+        listEl.setAttribute("draggable", "true")
+        listEl.setAttribute("id", uuidv4())
+        listEl.classList.add("listItems")
+        listEl.addEventListener("click", handleTaskClick)
+        document.getElementById(bucketId).append(listEl)
+    }
+}
+
+function fetch(snapshot, bucketId) {
+    document.getElementById(bucketId).innerHTML = ""
+    if (snapshot.val()) {
+        let taskArray = Object.values(snapshot.val())
+        appendBucket(taskArray, bucketId)
+    }}
